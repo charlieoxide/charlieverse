996caa60dfd9ae21778aefe86fadc80740366cf4
charlieoxide <kolidarshan890@gmail.com>
2025-06-26 02:41:09 -0500

    Add full project files

--- server/db.ts
+++ server/db.ts
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import { Pool, neonConfig } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-serverless';
 import ws from "ws";
@@ -6,9 +9,7 @@ import * as schema from "../shared/schema";
 neonConfig.webSocketConstructor = ws;
 
 if (!process.env.DATABASE_URL) {
-  throw new Error(
-    "DATABASE_URL must be set. Did you forget to provision a database?",
-  );
+  throw new Error("DATABASE_URL must be set. Did you forget to provision a database?");
 }
 
 export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
